041cbc33f0ff3efa9ab07d11aa33c7ddab58f707
occitaweb <micmc422@gmail.com>
2025-06-24 00:33:17 +0200

    Create pages-index.tsx

---
+++ headless/pages-index.tsx
@@ -0,0 +1,17 @@
+export async function getStaticProps() {
+  const res = await fetch('https://votre-site.com/wp-json/wp/v2/posts');
+  const posts = await res.json();
+
+  return {
+    props: { posts },
+    revalidate: 120, // ISR : régénération toutes les 2 minutes
+  };
+}
+
+export default function Home({ posts }) {
+  return (    
+      {posts.map(post => (        
+          {post.title.rendered}
+      ))}    
+  );
+}
