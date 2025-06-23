1853ae0056268e518ae5d3f5a583db4c8f785420
occitaweb <micmc422@gmail.com>
2025-06-24 00:34:46 +0200

    Create app-route-page.tsx

---
+++ headless/app-route-page.tsx
@@ -0,0 +1,17 @@
+export default async function Home() {
+  const res = await fetch('https://votre-site.com/wp-json/wp/v2/posts', {
+    next: { revalidate: 120 } // ISR automatique (rebuild toutes les 2 minutes)
+  });
+  const posts = await res.json();
+
+  return (
+    <main>
+      {posts.map(post => (
+        <article key={post.id}>
+          <h2 dangerouslySetInnerHTML={{ __html: post.title.rendered }} />
+          <div dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }} />
+        </article>
+      ))}
+    </main>
+  );
+}
